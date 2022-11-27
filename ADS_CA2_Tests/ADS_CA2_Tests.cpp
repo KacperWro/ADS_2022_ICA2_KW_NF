04ad32b96d6e2a8c78f11fb431437b7b76da42e6
KacperW <73821466+KacperWro@users.noreply.github.com>
2022-11-27 13:56:11 +0000

    Uploaded test project folder

---
+++ ADS_CA2_Tests/ADS_CA2_Tests.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ADSCA2Tests
+{
+	TEST_CLASS(ADSCA2Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//Test code here
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			//Test code here
+		}
+	};
+}
