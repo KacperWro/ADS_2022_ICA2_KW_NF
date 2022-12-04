24f6e4688c365cdc45822a0281eab64303cf85d0
Kacper <adamczyk911119@gmail.com>
2022-12-04 16:25:46 +0000

    Implemented functionality for inserting new nodes into the binary tree

--- ADS_CA2_Tree/ADS_CA2_Tree.cpp
+++ ADS_CA2_Tree/ADS_CA2_Tree.cpp
@@ -7,6 +7,7 @@
 #include "Utilities.h"
 #include "StudentKey.h"
 #include "Student.h"
+#include "BinaryTree.h"
 
 using namespace std;
 
@@ -20,7 +21,9 @@ int main()
 		cout << "Something went wrong with initialisation. ERROR CODE: " << x << endl;
 	}
 	
-	vector<vector<string>> delimitedRows = readDelimitedRows("C://Users//nfeda//source//repos//ADS_2022_ICA2_KW_NF//ADS_CA2_Tree//data_1000.csv");
+	vector<vector<string>> delimitedRows = readDelimitedRows("C://Users//Kacper//source//repos//ADS_2022_ICA2_KW_NF//ADS_CA2_Tree//data_1000.csv");
+
+	BinaryTree<StudentKey, Student> newTree;
 
 	for (int i = 1; i < delimitedRows.size(); i++) {
 		string timeDelimiter = ":";
@@ -46,46 +49,9 @@ int main()
 			delimitedRows[i][6], delimitedRows[i][7], delimitedRows[i][8], delimitedRows[i][9], lastLogOnDate, lastLogOnTime, joinedOn, delimitedRows[i][13]);
 
 		StudentKey newKey(newStudent.getUser_id(), newStudent.getFirst_name(), newStudent.getEmail());
-		TNode<StudentKey, Student> newNode(newKey, newStudent);
+		/*TNode<StudentKey, Student> newNode(newKey, newStudent);*/
 
-
-		if (i == 7) {
-			Date finalDob(delimitedRows[100][5]);
-
-			//LAST LOG ON DATE
-			Date finalLastLogDate(delimitedRows[100][10]);
-
-			//LAST LOG ON TIME
-			string finalLastLogTime = delimitedRows[100][11];
-			unsigned short int lastHours = stoi(finalLastLogTime.substr(0, finalLastLogTime.find(timeDelimiter)));
-			finalLastLogTime = finalLastLogTime.substr(3);
-			unsigned short int lastMinutes = stoi(finalLastLogTime.substr(0, finalLastLogTime.find(timeDelimiter)));
-
-			TimeHHMM lastLogOnTime(lastHours, lastMinutes);
-
-			//JOINED ON
-			Date lastJoinedOn(delimitedRows[100][12]);
-
-			Student newerStudent(delimitedRows[100][0], stoi(delimitedRows[100][1]), delimitedRows[100][2], delimitedRows[100][3], delimitedRows[100][4], finalDob,
-				delimitedRows[100][6], delimitedRows[100][7], delimitedRows[100][8], delimitedRows[100][9], finalLastLogDate, lastLogOnTime, lastJoinedOn, delimitedRows[100][13]);
-
-			StudentKey newerKey(newerStudent.getUser_id(), newerStudent.getFirst_name(), newerStudent.getEmail());
-			bool truth = newNode.insert(newerKey, newerStudent);
-			cout << truth;
-
-			cout << "\n" << newNode.getData().getEmail() << endl;
-			TNode<StudentKey, Student> *newestNode = newNode.getpRight();
-			cout << "\n" << newestNode->getData().getEmail() << endl;
-
-			break;
-		}
-
-		/*for (auto it = delimitedRows[i].begin(); it != delimitedRows[i].end(); it++)
-
-			cout << *it << " " << endl;
-		cout << endl;*/
-
-		
+		newTree.insert(newKey, newStudent);
 
 	}
 
--- ADS_CA2_Tree/BinaryTree.h
+++ ADS_CA2_Tree/BinaryTree.h
@@ -6,16 +6,63 @@
 //then K = StudentKey(email, dob, address)
 // create hash number from three selected fields and use as Key in node object
 //hashcode method for Key (gives unique number)
+#include <string>
+#include "TNode.h"
+#include "Student.h"
+#include "StudentKey.h"
 
 template <typename K, typename E>
 class BinaryTree
 {
 public:
+	TNode<StudentKey, Student>* root;
 
-	BinaryTree();
+	BinaryTree()
+	{
+		root = nullptr;
+	}
 
-	E* search(K key) {
+	/*E* search(K key) {
 		return nullptr;
-	};
-};
+	}*/
 
+	// Currently not 100% sure whether to make this one return a true/false value or a pointer
+	// If such an object exists then the insertion won't happen, but it it's already there
+	// Do we really care if the insertion happened or not?
+	// do we provide both a key and a full object? for example, use the key's hash for navigation
+	// and then attach the data object to the tree?
+
+	/*E* insert(K key, E dataObj) {
+		return nullptr;
+	}*/
+
+	void insert(K key, E data)
+	{
+		if (root == nullptr)
+		{
+			cout << "\nroot pointer is null" << endl;
+			root = new TNode<StudentKey, Student>(key, data);
+		}
+		else
+		{
+			cout << "\nroot pointer is not null" << endl;
+			root->insert(key, data);
+		}
+	}
+
+
+
+	// Pretty sure this one would require a search first, then a recursive deletion algorithm
+	// similar to the count algorithm... but that is only if nodes are dynamically allocated
+	// if they are not recursively allocated, wouldn't c++ just clean them up itself?
+	// return false could signify the key wasnt in the tree or key was entered wrong by user
+	//bool deleteSubTree(K key) {
+	//	return false;
+	//}
+
+	//// Return a string or make this one as an ostream???
+	//string printTree(size_t depth) {
+	//	string compileStr = "";
+	//	return compileStr;
+	//}
+};
